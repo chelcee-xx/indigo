d0bc8865ce0535f1ba8be76db9a4b8d157d6077c
chelcee-xx <chelcee1614@gmail.com>
2023-01-28 19:50:39 +0100

    changes made

--- src/sections/Cta.js
+++ src/sections/Cta.js
@@ -14,7 +14,7 @@ export default function Cta() {
         <img data-aos="fade-up" src={menu} alt="background"></img>
         <div data-aos="fade-up"  data-aos-delay="300"className=" absolute top-[40%] left-0 right-0 flex items-center justify-center flex-col">
           {" "}
-          <h1 className="text-4xl font-['viga'] text-white">
+          <h1 className="text-6xl tex font-['viga'] text-white">
             EXPLORE OUR TASTY MENU
           </h1>
           <p className="text-center  w-[60%] font-light mt-7 mb-11">
@@ -22,7 +22,7 @@ export default function Cta() {
             praesent ac. Integer commodo mattis diam et nibh risus. Tellus nunc
             magna sapien praesent platea elementum lacus.{" "}
           </p>
-          <button className="border py-3 px-5 hover:bg-white hover:text-primary">View Full Menu</button>
+          <button className="border text-lg py-3 px-5 hover:bg-white hover:text-primary">View Full Menu</button>
         </div>
       </div>
     </>
--- src/sections/Reservations.js
+++ src/sections/Reservations.js
@@ -14,15 +14,15 @@ export default function Reservations() {
         <img data-aos="fade-up" src={menu} alt="background"></img>
         <div data-aos="fade-up" data-aos-delay="300" className=" absolute top-[40%] left-0 right-0 flex items-center justify-center flex-col">
           {" "}
-          <h1 className="text-4xl font-['viga'] text-white">
+          <h1 className="text-6xl font-['viga'] text-white">
             Let's Save a space for you
           </h1>
           <p className="text-center w-[60%] font-light mt-7 mb-11">
             Lorem ipsum dolor sit amet consectetur. Lobortis ut dictum elit
-            praesent ac. Integer commodo mattis diam et nibh risus. Tellus nunc
+            praesent ac. Integer commodo mattis 6diam et nibh risus. Tellus nunc
             magna sapien praesent platea elementum lacus.{" "}
           </p>
-          <button className="border py-3 px-5 hover:bg-white hover:text-primary">
+          <button className="border py-3 text-lg px-5 hover:bg-white hover:text-primary">
             Make Reservation
           </button>
         </div>
